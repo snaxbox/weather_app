6bf74f4a80e5adc8d432bbfe82bb7b57eab94620
snaxbox <kekeenglish2018@gmail.com>
2020-10-29 17:52:13 +0800

    update images/icons ui

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,16 @@ const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
 
 const updateUI = (data) => {
 
     const cityDets = data.cityDets;
-    
+    // const {cityDets} = data;
+
+    //update details template
     details.innerHTML = `
         <h5 class="my-3">${cityDets.name}</h5>
         <div class="my-3">${cityDets.weather[0].description}</div>
@@ -17,6 +22,23 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // update icon source
+    const iconSrc = `http://openweathermap.org/img/wn/${cityDets.weather[0].icon}.png`;
+    icon.setAttribute("src", iconSrc);
+
+    //update the night/day images
+    let timeSrc = null;
+    if (cityDets.dt >= cityDets.sys.sunset){
+        timeSrc = "img/night.svg";   
+    }else{
+        timeSrc = "img/day.svg";
+    }
+    time.setAttribute("src", timeSrc);
+
+
+
+
 
     //remove display:none for the card div
     if(card.classList.contains("d-none")){
